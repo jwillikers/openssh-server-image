361786cf633b50bbc5cd2ff9aec5f7e152037f76
Jordan Williams <jordan@jwillikers.com>
2021-06-15 17:46:32 -0500

    Initial commit

---
+++ openssh-server-image-minimal.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# Create a container
+CONTAINER=$(buildah from registry.fedoraproject.org/fedora-minimal:latest)
+
+buildah run "$CONTAINER" /bin/sh -c 'microdnf install -y openssh-server passwd shadow-utils --nodocs --setopt install_weak_deps=0'
+
+buildah run "$CONTAINER" /bin/sh -c 'microdnf clean all -y'
+
+buildah run "$CONTAINER" /bin/sh -c 'echo "LogLevel DEBUG2" > /etc/ssh/sshd_config.d/99-clion.conf'
+
+buildah run "$CONTAINER" /bin/sh -c 'echo "PermitRootLogin yes" >> /etc/ssh/sshd_config.d/99-clion.conf'
+buildah run "$CONTAINER" /bin/sh -c 'echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config.d/99-clion.conf'
+buildah run "$CONTAINER" /bin/sh -c 'echo "PermitEmptyPasswords yes" >> /etc/ssh/sshd_config.d/99-clion.conf'
+# buildah run "$CONTAINER" /bin/sh -c 'echo "Subsystem sftp /usr/lib/openssh/sftp-server" >> /etc/ssh/sshd_config.d/99-clion.conf'
+buildah run "$CONTAINER" /bin/sh -c 'mkdir /run/sshd'
+
+buildah run "$CONTAINER" /bin/sh -c 'ssh-keygen -A'
+
+buildah run "$CONTAINER" /bin/sh -c 'useradd -ms /bin/bash user'
+
+buildah run "$CONTAINER" /bin/sh -c 'echo password | passwd --stdin user'
+
+buildah config --port 22 "$CONTAINER"
+
+buildah config --cmd "/usr/sbin/sshd -D -e" "$CONTAINER"
+
+buildah commit "$CONTAINER" openssh-server
+
+buildah rm "$CONTAINER"
+
---
+++ openssh-server-image.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# Create a container
+CONTAINER=$(buildah from scratch)
+
+# Mount the container filesystem
+MOUNTPOINT=$(buildah mount "$CONTAINER")
+
+# glibc-minimal-langpack?
+dnf install -y --installroot "$MOUNTPOINT" --releasever 34 bash coreutils openssh-server passwd shadow-utils --nodocs --setopt install_weak_deps=False
+
+dnf clean all -y --installroot "$MOUNTPOINT" --releasever 34
+
+buildah unmount "$CONTAINER"
+
+buildah run "$CONTAINER" /bin/sh -c 'echo "LogLevel DEBUG2" > /etc/ssh/sshd_config.d/99-clion.conf'
+
+buildah run "$CONTAINER" /bin/sh -c 'echo "PermitRootLogin yes" >> /etc/ssh/sshd_config.d/99-clion.conf'
+buildah run "$CONTAINER" /bin/sh -c 'echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config.d/99-clion.conf'
+buildah run "$CONTAINER" /bin/sh -c 'echo "PermitEmptyPasswords yes" >> /etc/ssh/sshd_config.d/99-clion.conf'
+# buildah run "$CONTAINER" /bin/sh -c 'echo "Subsystem sftp /usr/lib/openssh/sftp-server" >> /etc/ssh/sshd_config.d/99-clion.conf'
+buildah run "$CONTAINER" /bin/sh -c 'mkdir /run/sshd'
+
+buildah run "$CONTAINER" /bin/sh -c 'ssh-keygen -A'
+
+buildah run "$CONTAINER" /bin/sh -c 'useradd -ms /bin/bash user'
+
+buildah run "$CONTAINER" /bin/sh -c 'echo password | passwd --stdin user'
+
+# Expose Port 22/tcp
+buildah config --port 22 "$CONTAINER"
+
+# Start sshd
+buildah config --cmd "/usr/sbin/sshd -D -e" "$CONTAINER"
+
+# Save the container to an image
+buildah commit --squash "$CONTAINER" openssh-server
+
+buildah rm "$CONTAINER"
+
